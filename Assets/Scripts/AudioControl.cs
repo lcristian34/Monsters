0b36067d40ac67e1ce8bf742b2a882f345e36683
Christian Lara <lcristian34@gmail.com>
2019-03-16 22:06:38 -0500

    first commit

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioControl : MonoBehaviour {
+
+	public Slider volumeSlider;
+
+	public AudioSource volumeAudio;
+
+
+	public void volumeController(){
+		volumeAudio.volume = volumeSlider.value;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/moveCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour {
+
+	public float speed;
+	public Transform cameraMount;
+	public AudioSource woosh;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, cameraMount.position, speed);
+		transform.rotation = Quaternion.Slerp (transform.rotation, cameraMount.rotation, speed);
+	}
+
+	public void setMount(Transform newMount){
+		//woosh.Play();
+		cameraMount = newMount;
+	}
+
+}
